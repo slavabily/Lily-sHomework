9bebac9be523675b6b5ef66b751c5f27bc93645e
slava bily <slavabily@me.com>
2021-12-30 17:55:24 +0100

    insufficient flow for swapping array members in dependance of ads diff value is made

--- LilysHomework/main.swift
+++ LilysHomework/main.swift
@@ -7,5 +7,24 @@
 
 import Foundation
 
-print("Hello, World!")
+func lilysHomework(arr: [Int]) -> Int {
+    var tempArr = arr
+    for i in 0..<tempArr.count {
+        var diffs = [Int]()
+        for j in 0..<tempArr.count {
+            let diff = abs(tempArr[j] - tempArr[i])
+            diffs.append(diff)
+        }
+//        print(diffs)
+        let swap1 = diffs.remove(at: diffs.firstIndex(of: 0)!)
+        let swap2 = diffs.firstIndex(of: diffs.min()!)! + 1
+        tempArr.swapAt(swap1, swap2)
+        print(tempArr)
+//        break
+    }
+     
+    return Int()
+}
+
+print(lilysHomework(arr: [7,15,12,3]))
 
